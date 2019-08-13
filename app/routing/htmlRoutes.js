d63d7031a300819bd2e9893be1c1e6e08cc7729e
HB <hbianan@gmail.com>
2019-08-12 22:53:43 -0400

    files updated

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,18 @@
+//setting the required module for this file
 var path = require('path');
 
+//exporting the html routing paths...
 module.exports = (app) =>  {
+
+    //requesting for the url path, '/survey'  
     app.get('/survey', (request, result) => {
+        //the result will be the survey.html file to display
         result.sendFile(path.join(__dirname + '/../public/survey.html'));
     });
-
+    
+    //using the root url path as a 'default'
     app.use((request, result) => {
+        //then loads the result, which is the home.html file
         result.sendFile(path.join(__dirname + '/../public/home.html'));
     });
 
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require("express"), 
     app = express(),
+    path = require('path'),
     PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, '/app/public')));
+
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-app.listen(PORT, function() {
+app.listen(PORT, () => {
     console.log("App listening on PORT: " + PORT);
 });
-  
+   
